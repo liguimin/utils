60ab381f7b36c259bd063e8d9575a75132bac000
liguimin <610148007@qq.com>
2018-08-25 10:29:10 +0800

    add .gitignore

---
+++ src/Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/25
+ * Time: 9:58
+ */
+
+namespace utils;
+
+
+class Test
+{
+    public function test(){
+
+    }
+}
---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/25
+ * Time: 9:58
+ */
+
+namespace utils;
+
+
+class test
+{
+
+}
